6b2a46f85b3e64786316a06fac375d348ece9993
suesdev <com.mail.dev.sue@gmail.com>
2024-01-24 09:33:21 +0900

    First Commit
    最初のコミット

---
+++ Src/Scene/TitleScene.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "SceneBase.h"
+class SceneManager;
+
+class TitleScene : public SceneBase
+{
+
+public:
+
+	// コンストラクタ
+	TitleScene(void);
+
+	// デストラクタ
+	~TitleScene(void);
+
+	void Init(void) override;
+	void Update(void) override;
+	void Draw(void) override;
+	void Release(void) override;
+
+private:
+
+	// 画像
+	int imgTitle_;
+
+};
